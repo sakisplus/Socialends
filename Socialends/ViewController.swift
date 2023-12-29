4e5808aae2e33caccf000230c4983e8db2ec2f3a
Thanasis Galanis <ath.galanis@gmail.com>
2023-12-29 19:23:34 +0200

    Initial Commit

---
+++ Socialends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socialends
+//
+//  Created by Thanasis Galanis on 29/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
