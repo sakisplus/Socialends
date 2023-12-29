e24e5f95aa7cf654580a1fbd3063ada3c0331d4a
Thanasis Galanis <ath.galanis@gmail.com>
2023-12-30 00:44:51 +0200

    Configure collection flow

--- Socialends/Controllers/FollowersListVC.swift
+++ Socialends/Controllers/FollowersListVC.swift
@@ -10,12 +10,13 @@ import UIKit
 class FollowersListVC: UIViewController {
     
     var username: String?
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        configViewController()
+        configCollectionView()
         loadFollowers()
-        
     }
     
     private func loadFollowers() {
@@ -29,5 +30,28 @@ class FollowersListVC: UIViewController {
                 print(followers)
             }
         }
+    }
+    
+    private func configViewController() {
+        view.backgroundColor = .systemYellow
+    }
+    
+    private func configCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createColFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBlue
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    private func createColFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minItemsSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minItemsSpacing * 2)
+        let itemWidth = availableWidth / 3
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        return flowLayout
     }
 }
