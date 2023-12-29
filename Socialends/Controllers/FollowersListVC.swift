6e891c2ed106a0aca638022bb926a4aea53d7938
Thanasis Galanis <ath.galanis@gmail.com>
2023-12-29 23:36:13 +0200

    Get user's github followers

--- Socialends/Controllers/FollowersListVC.swift
+++ Socialends/Controllers/FollowersListVC.swift
@@ -14,5 +14,19 @@ class FollowersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        loadFollowers()
+        
+    }
+    
+    private func loadFollowers() {
+        guard let username = self.username else { return }
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
+            guard let followers = followers else {
+                self.presentSEAlertOnMainThread(title: "Something went wrong", message: error!, buttonTitle: "OK")
+                return
+            }
+            print("followers: \(followers.count)")
+            print(followers)
+        }
     }
 }
