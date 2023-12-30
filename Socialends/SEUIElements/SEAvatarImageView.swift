cf5fd414d598932f4b4407790e0dd4bc16709ed8
Thanasis Galanis <ath.galanis@gmail.com>
2023-12-30 12:15:21 +0200

    Load follower avatar from url

--- Socialends/SEUIElements/SEAvatarImageView.swift
+++ Socialends/SEUIElements/SEAvatarImageView.swift
@@ -26,4 +26,24 @@ class SEAvatarImageView: UIImageView {
         image = avatarPlaceholder
         translatesAutoresizingMaskIntoConstraints = false
     }
+    
+    func downloadImage(from urlSting: String) {
+        print("downloadImage from \(urlSting)")
+        guard let url = URL(string: urlSting) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
+    }
 }
