34908605f92d6ae0eeb9961e6a4828abf99488da
Thanasis Galanis <ath.galanis@gmail.com>
2023-12-31 18:51:57 +0200

    Group project files

---
+++ Socialends/SEUIElements/Labels/SESecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  SESecondaryTitleLabel.swift
+//  Socialends
+//
+//  Created by Thanasis Galanis on 31/12/23.
+//
+
+import UIKit
+
+class SESecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ Socialends/SEUIElements/ViewControllers/SEUserInfoHeaderVC.swift
@@ -0,0 +1,15 @@
+//
+//  SEUserInfoHeaderVC.swift
+//  Socialends
+//
+//  Created by Thanasis Galanis on 31/12/23.
+//
+
+import UIKit
+
+class SEUserInfoHeaderVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
